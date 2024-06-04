d3bde9d8de3f7042c38c6e2b3fd59e7a4c95e96f
Jackson MacDonald <jacksonmacdonald@gmail.com>
2024-06-04 14:25:33 +0100

    Feat Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         RecipeDatabase.getSessionFactory().getSchemaManager().exportMappedObjects(true);
 
-        RecipeRepository reciperepo = new RecipeRepository();
-        reciperepo.getRecipesHigerThan();
+//        RecipeRepository reciperepo = new RecipeRepository();
+//        reciperepo.getRecipesHigerThan();
 
 
     }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,14 @@
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.sql.Date;
 import java.util.List;
 
 
 @Entity
+@Table(name="User")
 public class User {
 
     @Id
